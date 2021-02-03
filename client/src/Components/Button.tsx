9c69d30fac44a1a410b20e99437ea48bae3c513c
link00000000 <crandall.logan@gmail.com>
2021-02-03 18:04:35 -0500

    Disable user select of button

--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -12,7 +12,7 @@ export const Button: FunctionComponent<Props> = ({
     return (
         <button className={`focus:outline-none ${styles['button']}`}>
             <div
-                className={`px-6 py-4 uppercase font-extrabold border-black border-8 ${color} ${styles['button__content']}`}
+                className={`px-6 py-4 uppercase font-extrabold border-black border-8 select-none ${color} ${styles['button__content']}`}
             >
                 {children}
             </div>
