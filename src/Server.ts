bc17ce95d372db930ebd5deb1a121fd800e33100
link00000000 <crandall.logan@gmail.com>
2021-02-05 20:30:51 -0500

    Add comments to Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -21,6 +21,10 @@ export class Server {
 
     constructor(private logger: winston.Logger) {}
 
+    /**
+     * Initialize HTTP server and begin serving traffic
+     * @param port Port to listen on
+     */
     public start(port = this.defaultPort) {
         this.app.use(bodyParser.json())
         this.app.use(this.logTraffic.bind(this))
@@ -30,6 +34,12 @@ export class Server {
         this.onListening(port)
     }
 
+    /**
+     * Traffic logging middleware. Must bind current `this` when invoking
+     * @param req HTTP request
+     * @param _res HTTP response
+     * @param next Next method in express pipeline
+     */
     private logTraffic(
         req: express.Request,
         _res: express.Response,
@@ -42,6 +52,12 @@ export class Server {
         next()
     }
 
+    /**
+     * Event handler triggered when the server is started
+     * @param port Port that express is listening on
+     * @param host Host that express is listening on
+     * @param protocol Protocol that express is using, either `http` or `https`
+     */
     private onListening(port: number, host = 'localhost', protocol = 'https') {
         this.logger.info(`Express listening at ${protocol}://${host}:${port}`)
     }
