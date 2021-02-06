34cd0240ba517ad9a1abcf65e2adb67e2634998f
link00000000 <crandall.logan@gmail.com>
2021-02-05 20:13:25 -0500

    Move logs to logs/ directory

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,10 +1,14 @@
 import winston from 'winston'
+import appRootPath from 'app-root-path'
 
 export const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.timestamp(),
     transports: [
-        new winston.transports.File({ filename: 'cmpct.log', level: 'debug' }),
+        new winston.transports.File({
+            filename: 'cmpct.log',
+            dirname: appRootPath.resolve('logs'),
+            format: winston.format.simple()
+        }),
         new winston.transports.Console({
             level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
             format: winston.format.cli({ colors: { info: 'blue' } })
