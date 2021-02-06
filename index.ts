6fbbad7bc3e6bed14d3161712e9eddbad9de277d
link00000000 <crandall.logan@gmail.com>
2021-02-05 20:56:44 -0500

    Create Routes/ in src/

--- index.ts
+++ index.ts
@@ -1,44 +1,11 @@
 import { Server } from './src/Server'
 import { UrlManager } from './src/UrlManager'
-import { StatusCodes } from 'http-status-codes'
 import { logger } from './src/Logger'
+import { createRequestHandler } from './src/Routes/Create'
 
 const server = new Server(logger)
 const urlManager = new UrlManager(logger)
 
-/**
- * Response type for create endpoint
- */
-interface CreateReponse {
-    error?: string
-    data?: {
-        id: string
-    }
-}
-
-/**
- * Create new short url
- */
-server.routes.post<CreateReponse>('/create', async (req, res) => {
-    if (req.body.url === undefined) {
-        const response: CreateReponse = {
-            error: "Key 'url' required"
-        }
-
-        return res.status(StatusCodes.BAD_REQUEST).json(response)
-    }
-
-    try {
-        const shortUrl = await urlManager.createShortUrl(req.body.url as string)
-        const response: CreateReponse = {
-            data: { id: shortUrl }
-        }
-
-        return res.status(StatusCodes.OK).json(response)
-    } catch (error) {
-        const response: CreateReponse = { error }
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response)
-    }
-})
+server.routes.post('/create', createRequestHandler(urlManager))
 
 server.start()
