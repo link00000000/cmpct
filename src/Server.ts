c7aa5eca5e42c085ed10d77822dbeec902b83f36
link00000000 <crandall.logan@gmail.com>
2021-02-12 10:24:23 -0500

    Remove https support

--- src/Server.ts
+++ src/Server.ts
@@ -1,43 +1,24 @@
 import express = require('express')
 import http = require('http')
-import https = require('https')
-import fs = require('fs')
-import appRootPath, { resolve } from 'app-root-path'
+import { resolve } from 'app-root-path'
 import bodyParser from 'body-parser'
 import type winston from 'winston'
 import proxy from 'express-http-proxy'
 
 /**
- * An HTTPS server that uses express. Expects SSL files in `certs/` directory.
- * If there are no certs, run `npm run ssl-gencerts` (openssl required).
- * Routes can be added using the `routes` property before starting. Server is
- * started using `start` method.
+ * An HTTP server that uses express. Routes can be added using the `routes`
+ * property before starting. Server is started using `start` method.
  */
 export class Server {
     public api = express.Router()
 
     private app = express()
-    private server: https.Server | http.Server
+    private server = http.createServer(this.app)
     private readonly defaultPort = process.env.PORT
         ? parseInt(process.env.PORT)
         : 8080
 
-    constructor(private logger: winston.Logger) {
-        // Use https in development with self-signed certs
-        if (process.env.NODE_ENV === 'development') {
-            const SSL_OPTIONS = {
-                key: fs.readFileSync(
-                    appRootPath.resolve('certs/selfsigned.pem')
-                ),
-                cert: fs.readFileSync(
-                    appRootPath.resolve('certs/selfsigned.cert')
-                )
-            }
-            this.server = https.createServer(SSL_OPTIONS, this.app)
-        } else {
-            this.server = http.createServer(this.app)
-        }
-    }
+    constructor(private logger: winston.Logger) {}
 
     /**
      * Initialize HTTP server and begin serving traffic
@@ -57,11 +38,7 @@ export class Server {
         }
 
         this.server.listen(port)
-        this.onListening(
-            port,
-            'localhost',
-            process.env.NODE_ENV === 'development' ? 'https' : 'http'
-        )
+        this.onListening(port, 'localhost')
     }
 
     /**
@@ -86,9 +63,8 @@ export class Server {
      * Event handler triggered when the server is started
      * @param port Port that express is listening on
      * @param host Host that express is listening on
-     * @param protocol Protocol that express is using, either `http` or `https`
      */
-    private onListening(port: number, host = 'localhost', protocol = 'https') {
-        this.logger.info(`Express listening at ${protocol}://${host}:${port}`)
+    private onListening(port: number, host = 'localhost') {
+        this.logger.info(`Express listening at http://${host}:${port}`)
     }
 }
