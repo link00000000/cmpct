292577c3194383c9b5f46c4fe9ed616a4dabb5b9
link00000000 <crandall.logan@gmail.com>
2021-02-12 13:48:19 -0500

    Redirect shortened urls to target urls

--- client/src/Routes/Redirect.tsx
+++ client/src/Routes/Redirect.tsx
@@ -1,5 +1,7 @@
-import { FunctionComponent } from 'react'
-import { RouteComponentProps, RouteProps, useLocation } from 'react-router-dom'
+import { FunctionComponent, useEffect } from 'react'
+import { RouteComponentProps, useHistory } from 'react-router-dom'
+import { RedirectResponseBody } from '../../../src/Routes/Redirect'
+import axios from 'axios'
 
 interface RouteInfo {
     shortUrl: string
@@ -8,5 +10,33 @@ interface RouteInfo {
 type Props = RouteComponentProps<RouteInfo>
 
 export const Redirect: FunctionComponent<Props> = (props) => {
-    return <h1>{props.match.params.shortUrl}</h1>
+    const history = useHistory()
+
+    const handleError = (error: Error) => {
+        console.log(error)
+        history.replace('/')
+    }
+
+    useEffect(() => {
+        axios
+            .get<RedirectResponseBody>('/api/' + props.match.params.shortUrl)
+            .then(({ data: response }) => {
+                if (response.error) {
+                    return handleError(new Error(response.error))
+                }
+
+                if (!response.data) {
+                    return handleError(
+                        new Error('Empty data response from server')
+                    )
+                }
+
+                window.location.href = response.data.targetUrl
+            })
+            .catch((error) => {
+                handleError(error)
+            })
+    }, [])
+
+    return <></>
 }
