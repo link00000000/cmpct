ac4b1aee9a60dc9c59c757ba4283a1dcecf9ce0f
link00000000 <crandall.logan@gmail.com>
2021-02-12 19:24:41 -0500

    Remove unused options in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-    purge: ['./src/**/*.tsx'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {}
-    },
-    variants: {
-        extend: {}
-    },
-    plugins: []
+    /**
+     * This list should include any files in your project that reference any of
+     * your styles by name. For example, if you have a JS file in your project
+     * that dynamically toggles some classes in your HTML, you should make sure
+     * to include that file in this list.
+     */
+    purge: ['./src/**/*.tsx']
 }
