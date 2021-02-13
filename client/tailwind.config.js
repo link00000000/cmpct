745ba05c8f020644c26d9815a235c2c13596ebb3
link00000000 <crandall.logan@gmail.com>
2021-02-13 13:20:56 -0500

    Move all Button styles into tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     purge: ['./src/**/*.tsx'],
     theme: {
         boxShadow: {
+            none: 'none',
             sm: '4px 4px 0 0 rgba(19 19 19 / 15%)',
             DEFAULT: '8px 8px 0 0 rgba(19 19 19 / 15%)',
             md: '12px 12px 0 0 rgba(19 19 19 / 15%)',
@@ -15,7 +16,26 @@ module.exports = {
             xl: '24px 24px 0 0 rgba(19 19 19 / 15%)',
             '2xl': '32px 32px 0 0 rgba(19 19 19 / 15%)',
             '3xl': '44px 44px 0 0 rgba(19 19 19 / 15%)',
-            none: 'none'
+            hidden: '0 0 0 0 rgba(19 19 19 / 15%)',
+            'sm-dark': '4px 4px 0 0 #000',
+            dark: '8px 8px 0 0 #000',
+            'md-dark': '12px 12px 0 0 #000',
+            'lg-dark': '16px 16px 0 0 #000',
+            'xl-dark': '24px 24px 0 0 #000',
+            '2xl-dark': '32px 32px 0 0 #000',
+            '3xl-dark': '44px 44px 0 0 #000',
+            'hidden-dark': '0 0 0 0 #000'
+        },
+        extend: {
+            transitionTimingFunction: {
+                expo: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)'
+            },
+            spacing: {
+                '8px': '8px'
+            }
         }
+    },
+    variants: {
+        translate: ['active', 'group-hover']
     }
 }
