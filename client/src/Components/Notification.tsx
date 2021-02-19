2cbc3a0cd4d5a6141f553e0b8147a42e328211b9
link00000000 <crandall.logan@gmail.com>
2021-02-19 15:34:31 -0500

    Add ability for notifications to auto-dismiss

--- client/src/Components/Notification.tsx
+++ client/src/Components/Notification.tsx
@@ -1,4 +1,6 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect } from 'react'
+import { useTimer } from '../Hooks/Timer'
+import classNames from 'classnames'
 
 export enum NotificationType {
     info,
@@ -10,7 +12,9 @@ interface Props {
     type?: NotificationType
     canDismiss?: boolean
     show?: boolean
-    onClose?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+    duration?: number
+    autoDismiss?: boolean
+    onClose?: (event?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 export const Notification: FunctionComponent<Props> = ({
@@ -18,6 +22,8 @@ export const Notification: FunctionComponent<Props> = ({
     type = NotificationType.info,
     canDismiss = true,
     show = true,
+    duration = 5000,
+    autoDismiss = true,
     onClose
 }) => {
     const colors = {
@@ -26,36 +32,96 @@ export const Notification: FunctionComponent<Props> = ({
         [NotificationType.warning]: 'bg-yellow-500'
     }
 
-    return (
-        <div
-            className={`px-6 py-4 fixed w-full bottom-0 left-0 ${colors[type]} text-white font-sans font-bold text-lg text-center`}
-            style={{
-                transform: `translateY(${show ? '0' : '100%'})`,
-                transition: 'transform 200ms'
-            }}
-        >
-            <span>{children}</span>
+    const timer = useTimer(duration)
 
-            {canDismiss && (
-                <button
-                    className="w-5 absolute right-6 top-5"
-                    onClick={onClose}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+    useEffect(() => {
+        // On timer end
+        if (timer.isDone && onClose) {
+            timer.reset()
+            onClose()
+        }
+    }, [timer.isDone])
+
+    useEffect(() => {
+        // Start timer
+        if (show && autoDismiss) {
+            timer.reset()
+            timer.start()
+        }
+    }, [show])
+
+    const handleClose = () => {
+        timer.reset()
+        if (onClose) {
+            onClose()
+        }
+    }
+
+    return (
+        <>
+            {JSON.stringify(timer)}
+            <div
+                className={classNames(
+                    'px-6',
+                    'py-4',
+                    'fixed',
+                    'w-full',
+                    'bottom-0',
+                    'left-0',
+                    'text-white',
+                    'font-bold',
+                    'text-lg',
+                    'text-center',
+                    colors[type]
+                )}
+                style={{
+                    transform: `translateY(${show ? '0' : '100%'})`,
+                    transition: 'transform 200ms'
+                }}
+            >
+                <span>{children}</span>
+
+                {/* Close button */}
+                {canDismiss && (
+                    <button
+                        className="w-5 absolute right-6 top-5"
+                        onClick={handleClose}
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                </button>
-            )}
-        </div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                )}
+
+                {/* Progress bar */}
+                <div
+                    className={classNames(
+                        'absolute',
+                        'left-0',
+                        'bottom-0',
+                        'bg-black',
+                        'opacity-50',
+                        'h-8px',
+                        timer.isRunning && 'transition-all',
+                        'duration-1000',
+                        'ease-linear'
+                    )}
+                    style={{
+                        width:
+                            (timer.currentTime / (duration - 1000)) * 100 + '%'
+                    }}
+                ></div>
+            </div>
+        </>
     )
 }
