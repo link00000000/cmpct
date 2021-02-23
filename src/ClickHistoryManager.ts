10f68600149842048b4767c3aae56a5a8929b896
Mason C. Roberts <mcr66@zips.uakron.edu>
2021-02-22 20:22:29 -0500

    Change the default connection to be cmpct database

--- src/ClickHistoryManager.ts
+++ src/ClickHistoryManager.ts
@@ -41,7 +41,7 @@ export class ClickHistoryManager {
             mongoose.connect(process.env.MONGO_URL)
             this.mongoClient = mongoose.connection
         } else {
-            mongoose.connect('mongodb://localhost:27017/test')
+            mongoose.connect('mongodb://localhost:27017/cmpct')
             this.mongoClient = mongoose.connection
         }
 
