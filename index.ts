1912532b81fc472a9e2d1e3e65fa7d5c77f6a757
link00000000 <crandall.logan@gmail.com>
2021-02-27 12:06:59 -0500

    Remove spam on testId channel

--- index.ts
+++ index.ts
@@ -22,27 +22,3 @@ server.api.delete('/:shortUrlId', deleteRequestHandler(urlManager))
 
 // Start
 server.start(parseInt(process.env.PORT ?? String(8080)))
-
-// @FIXME This is test data that spams connected clients, should be
-// removed later
-function randRange(min: number, max: number) {
-    return Math.random() * (max - min) + min
-}
-
-setInterval(() => {
-    clickHistoryManager
-        .updateEntry('testId', {
-            ip: '127.0.0.1',
-            time: new Date().getTime(),
-            browser: 'None',
-            city: 'Akron',
-            country: 'United States',
-            state: 'Ohio',
-            os: 'Windows 10',
-            coordinates: {
-                longitude: randRange(-180, 180),
-                latitude: randRange(-90, 90)
-            }
-        })
-        .catch((error) => logger.error(error.message))
-}, 1000)
