ad2255d82bbeede7d87e0dcdd33bd494f28bfd8a
link00000000 <crandall.logan@gmail.com>
2021-03-05 17:29:13 -0500

    Add comments to RandomId

--- src/Utils/RandomId.ts
+++ src/Utils/RandomId.ts
@@ -4,4 +4,7 @@ const CHARACTER_SET =
     '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 const LENGTH = 6
 
+/**
+ * Generate a random ID from the custom character set
+ */
 export const RandomId = customAlphabet(CHARACTER_SET, LENGTH)
